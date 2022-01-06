e53f361441a675ffcdc8aa2ec4f0444478f1cd84
wmamani <wilsonfmamani@yahoo.com>
2022-01-05 22:35:46 -0500

    feat:lista en python

--- sesion01/variables.py
+++ sesion01/variables.py
@@ -1,1 +1,21 @@
-print("yesO")
+edades = [10,20,3,12,"edad", 14.5, False, [1,2]]
+print(edades[-1])
+
+curso = {
+    'nombre': 'backend',
+    'dificultad': 'dificil',
+    'skills': [
+        {
+            'nombre': 'base de datos',
+            'nivel': 'intermedio'
+        },
+        {
+            'nombre': 'q4m',
+            'nivel': 'avanzado'
+
+        }
+    ]
+
+}
+
+print(curso['dificultad'])
