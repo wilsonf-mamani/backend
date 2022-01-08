5d9745d408a4d447ce180202da868aa018f613eb
wmamani <wilsonfmamani@yahoo.com>
2022-01-07 21:52:54 -0500

    feat: creacion consultas

--- sesion03/simulador.py
+++ sesion03/simulador.py
@@ -1,4 +1,31 @@
 from faker import Faker
+from faker.providers import person, internet
+
 
 objFaker = Faker()
-print(objFaker.name())
+objFaker.add_provider(person)
+objFaker.add_provider(internet)
+
+
+# print(objFaker.first_name())
+# print(objFaker.last_name())
+# print(objFaker.free_email())
+# print(objFaker.name())
+
+# cursos = ['COMUNICACION', 'CTA', 'INGLES', 'FRENCH']
+
+# for curso in cursos:
+#     print(f"INSERT INTO CURSOS (nombre) VALUES ('{curso}');")
+
+# INSERT INTO personas (nombre, dni, fecha_nacimiento, sexo, estado, created_at) VALUES
+#                      ('Patricio', '12964757', '1991-08-01', 'otro', true, now());  
+print('INSERT INTO personas (nombre, apellidos, email) VALUES')
+for i in range(100):
+    myfirstname = objFaker.first_name()
+    mylastname = objFaker.last_name()
+    myemail = objFaker.free_email()
+
+    print(f"('{myfirstname}','{mylastname}','{myemail}'),")
+
+# INSERT INTO personas (nombre, dni, fecha_nacimiento, sexo, estado, created_at) VALUES
+#                      ('Patricio', '12964757', '1991-08-01', 'otro', true, now());  
