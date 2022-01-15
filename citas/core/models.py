b8812195e9c3c714ae85b85226fdb9edca574393
wmamani <wilsonfmamani@yahoo.com>
2022-01-14 22:58:49 -0500

    feat:actualizacion contenido

--- citas/core/models.py
+++ citas/core/models.py
@@ -1,9 +1,20 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 
 
 class PersonaModel(models.Model):
+    opcionesEstadoCivil = [
+        ('soltero', 'SOLTERO'),
+        ('casado', 'CASADO'),
+        ('viudo', 'VIUDO'),
+        ('divorciado', 'DIVORCIADO'),
+        ('complicado', 'COMPLICADO'),                        
+        ('no_especifica', 'NO_ESPECIFICA')
+    ]
+
+
     personaId = models.AutoField(
         primary_key=True,
         unique=True,
@@ -30,3 +41,24 @@ class PersonaModel(models.Model):
         null=False,
         db_column='email'
     )
+
+    personaFechaNacimiento = models.DateField(
+        db_column='fech_nac'
+    )
+
+    personaEstadoCivil = models.CharField(
+        choices=opcionesEstadoCivil,
+        db_column='estado_civil',
+        default='NO_ESPECIFICA'
+    )
+    
+
+    personaFoto = models.ImageField(
+        db_column='foto',
+        upload_to='personas/',
+        null=True,
+
+    )
+
+    class Meta:
+        
