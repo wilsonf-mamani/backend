045426386293b1405ff0e7c1d6b09ca7d5e8448e
wmamani <wilsonfmamani@yahoo.com>
2022-01-26 18:45:29 -0500

    Limpieza de la rama

--- citas/core/migrations/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import PersonaModel
-
-class PersonasSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PersonaModel
-        fields = '__all__'
