cd1a4ae339f1dcc1150c3e65a8517cd8b23db5d0
wmamani <wilsonfmamani@yahoo.com>
2022-01-26 22:10:37 -0500

    avance

---
+++ prisma/seeds.js
@@ -0,0 +1,19 @@
+import Prisma from "@prisma/client";
+import adminSeed from "./seeds/admin.seed.js";
+
+const { PrismaClient} = Prisma;
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await Promise.all(adminSeed);
+}
+
+main()
+    .catch((e)=>{
+        console.log(e);
+        process.exit(1);
+    })
+    .finally(async()=>{
+        await prisma.$disconnect();
+    })
